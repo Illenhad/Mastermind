cba009d6e51e8cc2fcacf01e8de4f49c6d24de2e
Illenhad <pierrelegrand.pro@gmail.com>
2019-10-03 23:20:37 +0200

    modify design to game title

--- src/game.py
+++ src/game.py
@@ -1,15 +1,27 @@
+import os
+
+
 class Game:
     def __init__(self):
         self.color = ['yellow', 'blue', 'red', 'green', 'white', 'black', 'orange', 'violet']
         self.solution = []
         self.difficulty = 0
+        self.mod = 0
+
+        # verify OS to clear shell
+        if os.name == 'posix':
+            self.var_os = 'clear'
+        else:
+            self.var_os = 'cls'
 
     def set_color(self, n):
+
         if n == 2:
             self.color.append('pink')
             self.color.append('grey')
 
     def set_solution(self):
+
         while self.solution.count() <= 4:
             solution_choice = ''
 
@@ -18,19 +30,84 @@ class Game:
 
             self.append(solution_choice)
 
-    def gameTitle(self):
+    def game_title(self):
+        os.system(self.var_os)
+        print("")
         print(" ---------------------------------------------------")
-        print("|              Welcome to Mastermind !              |")
+        print("|{:^51}|".format("Welcome to Mastermind !"))
+        print("|{:51}|".format(""))
+        print("|{:51}|".format(" For a better experience, we recommend you"))
+        print("|{:51}|".format(" to run this program in a real terminal,"))
+        print("|{:51}|".format(" not an python interpreter."))
+        print(" ---------------------------------------------------")
+        input("Press Enter to continue...")
+
+        os.system(self.var_os)
+        print("")
+        print(" ---------------------------------------------------")
+        print("|{:^51}|".format("Player Name"))
         print(" ---------------------------------------------------")
 
-    def gameDifficulty(self):
+    def game_difficulty(self):
+        os.system(self.var_os)
+        print("")
         print(" ---------------------------------------------------")
-        print("|            1 : Normal    2 : Difficult            |")
+        print("|{:^51}|".format("1 : Normal    2 : Difficult"))
         print(" ---------------------------------------------------")
-        difficulty_choice = int(input(" Choose your difficulty : "))
 
-        while difficulty_choice not in (1, 2):
+        while True:
+            difficulty_choice = input(" Choose your difficulty : ")
+
+            if len(difficulty_choice) > 0:
+                if int(ord(difficulty_choice)) in (49, 50):
+                    break
+
             print(" Enter 1 for Normal or 2 for Difficult.")
-            difficulty_choice = int(input(" Choose your difficulty : "))
 
-        self.difficulty = difficulty_choice
+        self.difficulty = int(difficulty_choice)
+
+    def game_mod(self):
+        res = False
+
+        os.system(self.var_os)
+        print("")
+        print(" ---------------------------------------------------")
+        print("|{:^51}|".format("1 : Normal    2 : Debug"))
+        print(" ---------------------------------------------------")
+
+        while True:
+            mod_choice = input(" Choose your mod : ")
+
+            if len(mod_choice) > 0:
+                if int(ord(mod_choice)) in (49, 50):
+                    break
+
+            print(" Enter 1 for Normal or 2 for Debug.")
+
+        self.mod = int(mod_choice)
+
+    def game_title_resume(self, p1, p2):
+        os.system(self.var_os)
+
+        if self.difficulty == 1:
+            difficulty_choice = "Normal"
+        else:
+            difficulty_choice = "Difficult"
+
+        if self.mod == 1:
+            mod_choice = "Normal"
+        else:
+            mod_choice = "Difficult"
+
+        print("")
+        print(" ---------------------------------------------------")
+        print("|{:^51}|".format("Resume Game"))
+        print(" ---------------------------------------------------")
+        print("|{:8}{:10}{:>24}{:8}|".format("", "Player 1 : ", p1, ""))
+        print("|{:8}{:10}{:>24}{:8}|".format("", "Player 2 : ", p2, ""))
+        print("|{:51}|".format(""))
+        print("|{:8}{:14}{:>21}{:8}|".format("", "Difficulty : ", difficulty_choice, ""))
+        print("|{:8}{:14}{:>21}{:8}|".format("", "Mod : ", mod_choice, ""))
+        print(" ---------------------------------------------------")
+        input("Press Enter to continue...")
+
--- src/mastermind.py
+++ src/mastermind.py
@@ -3,16 +3,19 @@ from game import Game
 
 
 def main():
-    gameBoard = Game()
-    gameBoard.gameTitle()
+    game_board = Game()
+    game_board.game_title()
 
     player_one = Player(1)
     player_one.set_name()
 
-    player_one = Player(2)
-    player_one.set_name()
+    player_two = Player(2)
+    player_two.set_name()
 
-    gameBoard.gameDifficulty()
+    game_board.game_difficulty()
+    game_board.game_mod()
+
+    game_board.game_title_resume(player_one.name, player_two.name)
 
 
 if __name__ == '__main__':
