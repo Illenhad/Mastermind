fdb9a1b9619f7597124a60b54579dd9e4757ec72
Illenhad <pierrelegrand.pro@gmail.com>
2019-10-04 14:48:04 +0200

    gestion du board game

--- src/game.py
+++ src/game.py
@@ -4,7 +4,7 @@ import os
 class Game:
     def __init__(self):
         self.color = ['yellow', 'blue', 'red', 'green', 'white', 'black', 'orange', 'violet']
-        self.solution = []
+        self.solution = ["R", "G", "B", "Y"]
         self.difficulty = 0
         self.mod = 0
 
@@ -14,16 +14,6 @@ class Game:
         else:
             self.var_os = 'cls'
 
-    def set_solution(self):
-
-        while self.solution.count() <= 4:
-            solution_choice = ''
-
-            while not (solution_choice in self.color):
-                solution_choice = input('Enter a color \n')
-
-            self.append(solution_choice)
-
     def game_title(self):
         os.system(self.var_os)
         print("")
@@ -32,7 +22,7 @@ class Game:
         print("|{:51}|".format(""))
         print("|{:51}|".format(" For a better experience, we recommend you"))
         print("|{:51}|".format(" to run this program in a real terminal,"))
-        print("|{:51}|".format(" not an python interpreter."))
+        print("|{:51}|".format(" not a python interpreter."))
         print(" ---------------------------------------------------")
         input("Press Enter to continue...")
 
@@ -108,3 +98,66 @@ class Game:
         print("|{:8}{:14}{:>21}{:8}|".format("", "Mod : ", mod_choice, ""))
         print(" ---------------------------------------------------")
         input("Press Enter to continue...")
+
+    def game_board_view(self, color_choice_save, color_result_save):
+        x = 0
+        os.system(self.var_os)
+
+        if self.difficulty == 1:
+            head = "╔" + "═" * 33 + "╦" * 2 + "═" * 16 + "╗"
+            middle = "╠" + "═" * 33 + "╬" * 2 + "═" * 16 + "╣"
+            foot = "╚" + "═" * 33 + "╩" * 2 + "═" * 16 + "╝"
+            print(head)
+
+            for color_choice in color_choice_save:
+                print("║ {:^7}|{:^7}|{:^7}|{:^7} ║║ {:^3}|{:^3}|{:^3}|{:^3}║".format(color_choice[0],
+                                                                                     color_choice[1],
+                                                                                     color_choice[2],
+                                                                                     color_choice[3],
+                                                                                     color_result_save[x][0],
+                                                                                     color_result_save[x][1],
+                                                                                     color_result_save[x][2],
+                                                                                     color_result_save[x][3]))
+                print(middle)
+                x = x + 1
+
+            for i in range(0, 10 - x):
+                print("║ {0:^7}|{0:^7}|{0:^7}|{0:^7} ║║ {0:^3}|{0:^3}|{0:^3}|{0:^3}║".format(""))
+                print(middle)
+
+            if self.mod == 2:
+                print(
+                    "║ {:^7}|{:^7}|{:^7}|{:^7} ║║ {:^15}║".format(self.solution[0], self.solution[1], self.solution[2],
+                                                                  self.solution[3], "SOLUTION"))
+            else:
+                print("║ {0:^7}|{0:^7}|{0:^7}|{0:^7} ║║ {1:^15}║".format("x", "SOLUTION"))
+
+        else:
+            head = "╔" + "═" * 49 + "╦" * 2 + "═" * 24 + "╗"
+            middle = "╠" + "═" * 49 + "╬" * 2 + "═" * 24 + "╣"
+            foot = "╚" + "═" * 49 + "╩" * 2 + "═" * 24 + "╝"
+            print(head)
+
+            for color_choice in color_choice_save:
+                print("║ {:^7}|{:^7}|{:^7}|{:^7}|{:^7}|{:^7} ║║ {:^3}|{:^3}|{:^3}|{:^3}|{:^3}|{:^3}║"
+                      .format(color_choice[0], color_choice[1], color_choice[2], color_choice[3], color_choice[4],
+                              color_choice[5], color_result_save[x][0], color_result_save[x][1],
+                              color_result_save[x][2], color_result_save[x][3], color_result_save[x][4],
+                              color_result_save[x][5]))
+                print(middle)
+                x = x + 1
+
+            for i in range(0, 10 - x):
+                print("║ {0:^7}|{0:^7}|{0:^7}|{0:^7}|{0:^7}|{0:^7} ║║ {0:^3}|{0:^3}|{0:^3}|{0:^3}|{0:^3}|{0:^3}║"
+                      .format(""))
+                print(middle)
+
+            if self.mod == 2:
+                print(
+                    "║ {:^7}|{:^7}|{:^7}|{:^7} ║║ {:^15}║".format(self.solution[0], self.solution[1], self.solution[2],
+                                                                  self.solution[3], self.solution[4], self.solution[5],
+                                                                  "SOLUTION"))
+            else:
+                print("║ {0:^7}|{0:^7}|{0:^7}|{0:^7}|{0:^7}|{0:^7} ║║ {1:^23}║".format("x", "SOLUTION"))
+
+        print(foot)
--- src/mastermind.py
+++ src/mastermind.py
@@ -3,6 +3,11 @@ from game import Game
 
 
 def main():
+    color_choice = ["R", "G", "R", "V", "R", "H"]
+    color_result = ["R", "W", "W", "X", "X", "X"]
+    color_choice_save = [color_choice, color_choice, color_choice]
+    color_result_save = [color_result, color_result, color_result]
+
     game_board = Game()
     game_board.game_title()
 
@@ -17,6 +22,7 @@ def main():
 
     game_board.game_title_resume(player_one.name, player_two.name)
 
+    game_board.game_board_view(color_choice_save, color_result_save)
 
 if __name__ == '__main__':
     main()
